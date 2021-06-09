891557ddb05d9787145a8d03c6a1034f2cac99e0
vinhvn <vincentn337@gmail.com>
2021-06-08 20:28:41 -0400

    refactor: :zap: cleanup, added user storage and login route

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,8 @@ import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import multer from 'multer';
-import { v4 as uuid } from 'uuid';
+import randimals from 'randimals/dist';
+import fs from 'fs';
 
 /**
  * general setup and configuration
@@ -15,15 +16,41 @@ import { v4 as uuid } from 'uuid';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const NODE_ENV = process.env.NODE_ENV;
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+const DEMO_PASSWORD = process.env.DEMO_PASSWORD;
+const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 const APP_URL = process.env.APP_URL;
-const API_KEY = process.env.API_KEY;
+
 // get our local storage up and running
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'static'),
+  destination: (req, _, cb) => {
+    // get username and make directory
+    const username = req.header('Username');
+    // if admin, place in `/`
+    if (username === ADMIN_USERNAME) {
+      return cb(null, path.join(__dirname, 'data'));
+    }
+
+    const filePath = path.join(__dirname, 'data', username);
+    try {
+      fs.mkdirSync(filePath, { recursive: true });
+    } catch (e) {
+      // in case directory already exists
+      if (e.code !== 'EEXIST') {
+        throw e;
+      }
+    }
+
+    cb(null, filePath);
+  },
   filename: (_, file, cb) => {
-    cb(null, uuid() + path.extname(file.originalname));
+    cb(
+      null,
+      randimals({ adjectives: 2, format: 'pascal' }) +
+        path.extname(file.originalname)
+    );
   },
 });
 const upload = multer({ storage });
@@ -32,8 +59,7 @@ const upload = multer({ storage });
  * express middleware
  */
 app.use(express.json());
-app.use('/', express.static(path.join(__dirname, 'static')));
-// allow CORS in our app
+app.use('/', express.static(path.join(__dirname, 'data')));
 app.use((_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -44,33 +70,10 @@ app.use((_, res, next) => {
 });
 
 /**
- * routes and handlers
- */
-app.get('/', (_, res) => {
-  res.redirect(APP_URL);
-});
-app.post('/upload', authenticate, upload.single('file'), (req, res) => {
-  console.log(
-    `[SERVER]: successfully uploaded file @ ${API_URL}/${req.file.filename}`
-  );
-  res.status(201).send(`${API_URL}/${req.file.filename}`);
-});
-// catch all other requests as 404s and redirect them to the app
-app.all('*', (req, res) => {
-  res.redirect(`${APP_URL}${req.url}`);
-});
-
-// start the express server
-app.listen(PORT, () => {
-  console.log(
-    `[SERVER]: started at ${
-      NODE_ENV === 'development' ? `http://localhost:${PORT}` : API_URL
-    }`
-  );
-});
-
-/**
  * the authenticate function is an express middleware used to check for a matching API key
+ * @param req   {Object} request
+ * @param res   {Object} response
+ * @param next  {Function} callback function
  */
 function authenticate(
   req: express.Request,
@@ -84,3 +87,41 @@ function authenticate(
     next();
   }
 }
+
+/**
+ * routes and handlers
+ */
+app.get('/', (_, res) => {
+  res.redirect(APP_URL);
+});
+
+app.post('/upload', authenticate, upload.single('file'), (req, res) => {
+  console.log(
+    `[SERVER]: successfully uploaded file @ ${API_URL}/${req.file.filename}`
+  );
+  res.status(201).send(`${API_URL}/${req.file.filename}`);
+});
+
+app.post('/login', (req, res) => {
+  const { password } = req.body;
+  if (!password) {
+    res.sendStatus(401);
+  }
+
+  if (password === ADMIN_PASSWORD) {
+    res.status(200).send('admin');
+  } else if (password === DEMO_PASSWORD) {
+    res.status(200).send('demo');
+  } else {
+    res.sendStatus(401);
+  }
+});
+// catch all other requests as 404s and redirect them to the app
+app.all('*', (req, res) => {
+  res.redirect(`${APP_URL}${req.url}`);
+});
+
+// start the express server
+app.listen(PORT, () => {
+  console.log(`[SERVER]: started at ${API_URL}`);
+});
